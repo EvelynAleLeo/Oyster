984541844e067391977772d830d33281f4e41cfd
Evelyn <evelyn@ipsoft.es>
2019-03-11 16:09:45 +0000

    Refactored Oystercard

--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -1,6 +1,10 @@
 require 'Oystercard.rb'
 
 describe Oystercard do
+  # In order to use public transport
+  # As a customer
+  # I want money on my card
+
   it 'has a balance of 0' do
     oystercard = Oystercard.new
     expect(oystercard.balance).to eq 0
